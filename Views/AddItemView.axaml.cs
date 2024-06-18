7d25dcf604936f886f793e130cf51c01cfad2cc8
DawidM3 <161303514+DawidM3@users.noreply.github.com>
2024-06-18 18:59:04 +0200

    Add files via upload

---
+++ Views/AddItemView.axaml.cs
@@ -0,0 +1,14 @@
+using Avalonia.Controls;
+
+namespace ToDoList.Views
+{
+    public partial class AddItemView : UserControl
+    {
+        public AddItemView()
+        {
+            InitializeComponent();
+            this.Width = 512;
+            this.Height = 328;
+        }
+    }
+}
---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,16 @@
+using Avalonia.Controls;
+
+namespace ToDoList.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.Width = 720;
+            this.Height = 480;
+
+        }
+    }
+}
